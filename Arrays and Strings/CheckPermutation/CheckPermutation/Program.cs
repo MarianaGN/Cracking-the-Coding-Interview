520162ea17224e71ee2422905a1ad6e025c81014
MarianaGN <mariana.kovalchuk@inveritasoft.com>
2018-10-30 18:57:09 +0200

    Created CheckPermutation exercise project

---
+++ Arrays and Strings/CheckPermutation/CheckPermutation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace CheckPermutation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first string: ");
+            var firstStr = Console.ReadLine();
+            firstStr = firstStr ?? string.Empty;
+
+            Console.WriteLine("Enter second string: ");
+            var secondStr = Console.ReadLine();
+            secondStr = secondStr ?? string.Empty;
+
+            var orderedFirstStr = firstStr.ToList().OrderBy(c => c).ToList();
+            var orderedSecondStr = secondStr.ToList().OrderBy(c => c).ToList();
+
+            var isSameLength = orderedFirstStr.Count == orderedSecondStr.Count;
+            var isEqual = isSameLength;
+
+            if (isSameLength)
+            {
+                var i = 0;
+                while (isEqual && i < orderedFirstStr.Count)
+                {
+                    isEqual = orderedFirstStr[i] == orderedSecondStr[i];
+                    i++;
+                }
+            }
+
+            var reportToUser = isEqual
+                ? "Two strings are permutation of the same string."
+                : "Two strings are not permutation of the same string.";
+
+            Console.WriteLine(reportToUser);
+            Console.ReadLine();
+        }
+    }
+}
